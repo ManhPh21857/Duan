76c6ee399167b28c5ed98bf66c71cd79d8a52d48
ANTHONYPC\Anthony <AnthonyNguyenReal@gmail.com>
2022-11-29 20:34:47 +0700

    form 2

---
+++ DuAn1/1.DAL/Extensions/ModelbuilderExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1.DAL.Models;
+using Amazon.SimpleWorkflow.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1.DAL.Extensions
+{
+    public static class ModelbuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BangSanPham>().HasData(
+
+               new BangSanPham() {  MaSP = "c3ef7dfc-8915-437e-8897-deb4288c3574", TenSP = "Giày Thể Thao"},
+               new BangSanPham() { MaSP = "f76ee3e7-e818-49cc-822e-a7142192dbce", TenSP = "Giày Du Mục"},
+               new BangSanPham() {  MaSP = "d68187f0-f714-4321-8be2-c1371cb8a13a", TenSP = "Giày Hiệp Sỹ"},
+               new BangSanPham() {  MaSP = "7df9f624-80e0-458c-b6be-3024d6bc6bc6", TenSP = "Giày Kiên Cường"},
+               new BangSanPham() {  MaSP = "6c9e926b-c97d-4922-aca8-909831fb6a59", TenSP = "Giày Hộ Vệ"}
+                ) ;
+            modelBuilder.Entity<BangLoaiSP>().HasData(
+                new BangLoaiSP() { MaLoai = "3ea19778-dde3-480c-8620-e70ec427de35", TenLoai = "Giày Thể Thao" },
+                new BangLoaiSP() { MaLoai = "a6ae2bf5-7a49-4d74-b160-b75ba2e987bb", TenLoai = "Giày Du Mục" },
+                new BangLoaiSP() { MaLoai = "3b171b18-96f0-43e3-9ca4-711189825354", TenLoai = "Giày Hiệp Sỹ" },
+                new BangLoaiSP() { MaLoai = "bdb2ffc7-8e3c-47f1-bfae-db4fe6e1a0b7", TenLoai = "Giày Kiên Cường" },
+                new BangLoaiSP() { MaLoai = "2e2ac9a2-e086-4f0f-8bd5-161ce627b34b", TenLoai = "Giày Hộ Vệ" }
+            );
+            modelBuilder.Entity<BangMauSac>().HasData(
+                    new BangMauSac() { MaMs = "5918e7bf-c8ff-4151-977a-01fa54a68d27", TenMs = "Xanh" },
+                    new BangMauSac() { MaMs = "6c3c542e-5d95-4c2f-a004-27bf440f845c", TenMs = "Đỏ" },
+                    new BangMauSac() { MaMs = "b0320323-9a80-4b33-bb9d-d6b829039a64", TenMs = "Tím" },
+                    new BangMauSac() { MaMs = "4f09c127-8991-4881-9ee4-9d801c0776f2", TenMs = "Vàng" },
+                    new BangMauSac() { MaMs = "ad0ed8ac-9de9-448b-9f5a-b1cd4e4c5e06", TenMs = "Trắng" }
+                ) ;
+            modelBuilder.Entity<BangHangSanPham>().HasData(
+                    new BangHangSanPham() { MaHang = "14e051b1-0601-415d-be03-241d60663a89", TenHang= "Adidas"},
+                    new BangHangSanPham() { MaHang = "bf715726-612b-4039-9ed9-d4b4175aaf8c", TenHang= "Puma"},
+                    new BangHangSanPham() { MaHang = "bf76abab-09e8-45cc-9814-a3e9b1cd8130", TenHang= "Nike"},
+                    new BangHangSanPham() { MaHang = "61890671-2af1-4445-8129-f5db8f91fc66", TenHang= "Thượng Đỉnh"},
+                    new BangHangSanPham() { MaHang = "168ef6d4-fa49-45f0-9b1d-e9173523a927", TenHang= "Gucci"}
+                );
+            modelBuilder.Entity<BangChucVu>().HasData(
+                    new BangChucVu() { MaCV = "CV01" , TenCV = "Quản Lý"},
+                    new BangChucVu() { MaCV = "CV02" , TenCV = "Nhân Viên"}
+                );
+            modelBuilder.Entity<BangKhuyenMai>().HasData(
+                    new BangKhuyenMai() { MaKM = "b9c838ee-53f0-4b41-a39e-46c27585afc0", MotaKM = "Mừng Bạn Mới" },
+                    new BangKhuyenMai() { MaKM = "6161cfad-6e92-4a8e-ae45-43542500af79", MotaKM = "Black friday" },
+                    new BangKhuyenMai() { MaKM = "9977ec62-aa63-4a5a-b291-e743dd165e57", MotaKM = "Sale khai trương cửa hàng" },
+                    new BangKhuyenMai() { MaKM = "11756abf-cdda-42ec-a283-ab9c0db6bf1b", MotaKM = "Sale Cuối Năm" },
+                    new BangKhuyenMai() { MaKM = "44b40f2c-bed0-42f7-88c1-97d8ef744190", MotaKM = "Sale Xả Kho" }
+                );
+            modelBuilder.Entity<BangKhachHang>().HasData(
+                    new BangKhachHang() { Ma = "7eb6cc4a-0c52-4209-b86a-59583cc73eba", Ho = "Nguyễn",TenDem = "Thị",Ten = "Huyền",SDT = "0325145698",DiaChi = "Cầy Giấy - Hà Nội",GioiTinh = "Nữ",MatKhau = "111"},
+                    new BangKhachHang() { Ma = "fb9d708c-6b88-46a1-b97c-547bea5b3785", Ho = "Trần",TenDem = "Thị",Ten = "Trang",SDT = "0235448521",DiaChi = "Thanh Xuân - Hà Nội",GioiTinh = "Nữ",MatKhau = "222"},
+                    new BangKhachHang() { Ma = "03e9b177-72b6-42b2-85e9-9f1d0a74a76c", Ho = "Lê",TenDem = "Văn",Ten = "Thinh",SDT = "0385216265",DiaChi = "Hoàng Mai - Hà Nội",GioiTinh = "Nam",MatKhau = "333"},
+                    new BangKhachHang() { Ma = "04dc71db-f988-4b1a-8464-7392f8199534", Ho = "Hoàng",TenDem = "Văn",Ten = "Đại",SDT = "0521635842",DiaChi = "Minh Khai - Hà Nội",GioiTinh = "Nam",MatKhau = "444"},
+                    new BangKhachHang() { Ma = "e0d66516-9595-4d89-b2f8-0f3ef8bca686", Ho = "Vũ",TenDem = "Thị",Ten = "Hoa",SDT = "0693215479",DiaChi = "Đống Đa - Hà Nội",GioiTinh = "Nữ",MatKhau = "555"}
+                );
+            modelBuilder.Entity<ba>
+
+        }
+    }
+}
--- DuAn1/3.PL/Views/frmDangNhap.Designer.cs
+++ DuAn1/3.PL/Views/frmDangNhap.Designer.cs
@@ -30,76 +30,110 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.txb_tendangnhap = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txb_matkhau = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(384, 50);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(343, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(172, 41);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Đăng nhập";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(134, 133);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_dangnhap.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_dangnhap.Location = new System.Drawing.Point(245, 336);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(203, 78);
+            this.btn_dangnhap.TabIndex = 3;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = false;
+            // 
+            // txb_tendangnhap
+            // 
+            this.txb_tendangnhap.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txb_tendangnhap.Location = new System.Drawing.Point(245, 133);
+            this.txb_tendangnhap.Name = "txb_tendangnhap";
+            this.txb_tendangnhap.Size = new System.Drawing.Size(270, 43);
+            this.txb_tendangnhap.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label4.Location = new System.Drawing.Point(33, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 38);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tên đăng nhập";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_3.PL.Properties.Resources.logo_shop_giay_dep;
+            this.pictureBox1.Location = new System.Drawing.Point(545, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 320);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(134, 224);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label3.Location = new System.Drawing.Point(33, 214);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mật Khẩu";
             // 
-            // button1
+            // txb_matkhau
             // 
-            this.button1.Location = new System.Drawing.Point(319, 385);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(300, 133);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(300, 225);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
-            this.textBox2.TabIndex = 5;
+            this.txb_matkhau.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txb_matkhau.Location = new System.Drawing.Point(245, 211);
+            this.txb_matkhau.Name = "txb_matkhau";
+            this.txb_matkhau.Size = new System.Drawing.Size(270, 43);
+            this.txb_matkhau.TabIndex = 9;
             // 
             // frmDangNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(922, 502);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txb_matkhau);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txb_tendangnhap);
+            this.Controls.Add(this.btn_dangnhap);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frmDangNhap";
             this.Text = "frmDangNhap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +143,11 @@
 
         private Label label1;
         private Label label2;
+        private Button btn_dangnhap;
+        private TextBox txb_tendangnhap;
+        private Label label4;
+        private PictureBox pictureBox1;
         private Label label3;
-        private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox txb_matkhau;
     }
 }
